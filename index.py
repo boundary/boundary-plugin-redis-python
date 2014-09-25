11299199144fe3a09287969461e25bc496d29b43
Nitzan Miron <bug.assembla@bugbug.me>
2014-09-25 11:13:12 +0300

    * Fixed bug with param.json parsing. * Fixed aggregation issue with some parameters.

--- index.py
+++ index.py
@@ -6,9 +6,9 @@ import socket
 import boundary_plugin
 
 params = boundary_plugin.parse_params()
-redis_server = params.get('redis_hostname', 'localhost')
-redis_port = int(params.get('redis_port', 6379))
-redis_password = params.get('redis_password', None)
+redis_server = params.get('redis_hostname', None) or 'localhost'
+redis_port = int(params.get('redis_port', None) or 6379)
+redis_password = params.get('redis_password', None) or None
 
 if not redis_password:
     redis_password = None
@@ -22,29 +22,32 @@ def accum(stat_name, new_value):
     try:
         old_value = _accum[stat_name]
     except KeyError:
-        _accum[stat_name] = new_value
-        return new_value
+        old_value = new_value
 
     diff = new_value - old_value
     _accum[stat_name] = new_value
     return diff
 
+# List of values returned from Redis' info call to report to Boundary.
+# Each value has a boolean specifying whether to aggregate data (i.e. report
+# only delta from the previous value) or report the metric as-is.
 values_to_report = (
-    'connected_clients',
-    'keyspace_hits',
-    'keyspace_misses',
-    'expired_keys',
-    'evicted_keys',
-    'total_commands_processed',
-    'total_connections_received',
-    'used_memory_rss',
+    ('connected_clients', False),
+    ('keyspace_hits', True),
+    ('keyspace_misses', True),
+    ('expired_keys', True),
+    ('evicted_keys', True),
+    ('total_commands_processed', True),
+    ('total_connections_received', True),
+    ('used_memory_rss', False),
 )
 
 while True:
     info = r.info()
 
-    for v in values_to_report:
-        boundary_plugin.boundary_report_metric('REDIS_' + v.upper(), accum(v, info[v]))
+    for v,aggregate in values_to_report:
+        val = accum(v, info[v]) if aggregate else info[v]
+        boundary_plugin.boundary_report_metric('REDIS_' + v.upper(), val)
 
     boundary_plugin.sleep_interval()
 
